bb7600ae5f8194469c55de810234f58765b29462
Andrey Damasceno <andrey.damasceno@outlook.com>
2023-05-08 17:45:16 -0300

    novo projeto

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Filme filme1 = new Filme();
+        
+        filme1.nome = "Homem de Ferro";
+        filme1.anoDeLancamento = 2008;
+        filme1.duracaoEmMinutos = 180;
+
+        System.out.println(filme1);
+    }
+}
