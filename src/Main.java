2e307009bfa0c9c8ba6236d556f4639efc4b313e
Andrey Damasceno <andrey.damasceno@outlook.com>
2023-05-08 19:25:01 -0300

    novas modificações

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
+import br.com.alura.screenmatch.estudos.Filme;
 
 public class Main {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
         
-        filme1.nome = "Homem de Ferro";
-        filme1.anoDeLancamento = 2008;
-        filme1.duracaoEmMinutos = 180;
+        filme1.setNome("Homem de Ferro");
+        filme1.setAnoDeLancamento(2008);
+        filme1.setDuracaoEmMinutos(180);
+        filme1.avalia(9.4);
+        filme1.avalia(10);
+        filme1.avalia(9);
 
-        System.out.println(filme1);
+        filme1.exibeFichaTecnica();
+        System.out.println("Total de avaliações: " + filme1.getTotalDeAvaliacoes());
+        System.out.println("Nota do filme: " + filme1.obtemMediaDasAvaliacoes());
     }
 }
